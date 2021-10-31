ccf64700d7c5bcdeeaf2ee3d074734d1af20d2d4
Matthew Magin <matthew.d.magin@gmail.com>
2021-10-31 17:16:45 +1000

    Add if > 1 don't generate buttons

--- js/products_template.js
+++ js/products_template.js
@@ -1,39 +1,48 @@
 var santiseCategoryName = (text) => {
-    return text.replace("™","");
+  return text.replace("™", "");
 };
 
 var hideRows = (tableEl, filterClicked) => {
-    tableEl.forEach(row => {
-    let rowCategory = santiseCategoryName((row.dataset.category));
-    if(filterClicked != rowCategory){
-        row.parentElement.parentElement.style.display = "none";
+  tableEl.forEach((row) => {
+    let rowCategory = santiseCategoryName(row.dataset.category);
+    if (filterClicked != rowCategory) {
+      row.parentElement.parentElement.style.display = "none";
     } else {
-        row.parentElement.parentElement.style.display = "block";
+      row.parentElement.parentElement.style.display = "block";
     }
   });
 };
 
 var filterClick = (button) => {
-    let container = button.parentElement.parentElement
+  let container = button.parentElement.parentElement;
   let filterClicked = button.dataset.filter;
   let consumableTableRows = container.querySelectorAll(`.table-row .row-inner`);
-  container.querySelectorAll(`.filter-button.active`).forEach(btn => {
-      btn.classList.remove("active");
+  container.querySelectorAll(`.filter-button.active`).forEach((btn) => {
+    btn.classList.remove("active");
   });
   button.classList.add("active");
 
   hideRows(consumableTableRows, filterClicked);
-  };
+};
 
 let tableSetup = (filters, container, firstButtonName) => {
   filters = filters.split(",");
-  let filterOutput = document.querySelector(container);
+  if (filters.length > 1) {
+    let filterOutput = document.querySelector(container);
     filters.forEach((filter, i) => {
-      filter = filter.trim().replace("™","");
-      if(i==0 && firstButtonName == null) firstButtonName = filter;
-      let button = `<button class="button filter-button ${i==0 ? "active" : ""}" data-filter="${filter}" onclick="filterClick(this)">${i==0 ? firstButtonName : filter}</button>`;
+      filter = filter.trim().replace("™", "");
+      if (i == 0 && firstButtonName == null) firstButtonName = filter;
+      let button = `<button class="button filter-button ${
+        i == 0 ? "active" : ""
+      }" data-filter="${filter}" onclick="filterClick(this)">${
+        i == 0 ? firstButtonName : filter
+      }</button>`;
       filterOutput.innerHTML += button;
     });
-  
-  hideRows((filterOutput.parentElement).querySelectorAll(".table-row .row-inner"), santiseCategoryName(filters[0]));
-}
+
+    hideRows(
+      filterOutput.parentElement.querySelectorAll(".table-row .row-inner"),
+      santiseCategoryName(filters[0])
+    );
+  }
+};
